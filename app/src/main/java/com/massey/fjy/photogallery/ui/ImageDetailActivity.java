a4a55ba91275c2b6eae2cc76873eaa02d09b133d
changx03 <luke.x.chang@gmail.com>
2015-06-13 16:19:09 +1200

    my commit

--- app/src/main/java/com/massey/fjy/photogallery/ui/ImageDetailActivity.java
+++ app/src/main/java/com/massey/fjy/photogallery/ui/ImageDetailActivity.java
@@ -2,15 +2,18 @@ package com.massey.fjy.photogallery.ui;
 
 //import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -88,17 +91,42 @@ public class ImageDetailActivity extends FragmentActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                return true;
+            case R.id.action_edit:
+                showEditPopup(findViewById(item.getItemId()));
+                return true;
+            case R.id.action_delete:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showEditPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_edit_popup, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()) {
+                    case R.id.photo_filter:
+                        intent = new Intent(ImageDetailActivity.this,ImageEditActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.add_note:
+                        break;
+                    case R.id.tag_people:
+                        break;
+                }
+                return true;
+            }
+        });
+        popup.show();
     }
 
     @Override
